70d25f79270fcf64bc268839c481d03726841eac
Nwanne Nnamani <nnamaninwanne@gmail.com>
2025-09-07 19:25:19 +0100

    rewrite routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true, // keeps your trailing slashes
-
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        has: [
-          {
-            type: 'host',
-            value: 'brandx.temitoperuthjacob.com', // match subdomain
-          },
-        ],
-        destination: '/branding', // route only subdomain root to /branding
-      },
-    ]
-  },
 }
 
 export default nextConfig
